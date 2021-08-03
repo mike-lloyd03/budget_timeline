2034492e4ebd9c508aaa35ebc405157b578160b1
Mike Lloyd <mike.lloyd03@pm.me>
2021-08-02 23:08:13 -0700

    Add Plotly. Add calculate_date for calculating the date.

--- plot/plot_data.py
+++ plot/plot_data.py
@@ -1,4 +1,6 @@
-import pandas as pd
+from datetime import datetime
+from calendar import monthrange
+import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -9,24 +11,39 @@ def plot_data(data_list):
     """
     Plots a list of transaction dictionaries
     """
-    x_vals, y_vals = make_timeline(data_list)
+    x_vals, y_vals, _ = make_timeline(data_list)
     plt.plot(x_vals, y_vals)
     plt.xlabel("Date")
     plt.ylabel("Amount Available")
     plt.show()
 
 
-def make_timeline(data_list):
+def plot_data_plotly(data_list):
+    """
+    Plots data using Plotly
+    """
+    x_vals, y_vals, descriptions = make_timeline(data_list)
+    data = {"Day": x_vals, "Amount Available": y_vals, "Description": descriptions}
+    fig = px.line(data, "Day", "Amount Available", hover_name="Description")
+    fig.update_layout(yaxis_tickprefix="$")
+    fig.show()
+
+
+def make_timeline(data_list, start_date):
     """
     Converts a list of transaction dictionaries into lists of days and amounts.
     """
-    x_vals = []
-    y_vals = []
+    days = []
+    amounts = []
+    descriptions = []
+    dates = []
     for transaction in sorted(data_list, key=lambda t: t["day"]):
-        last_amount = y_vals[-1] if len(y_vals) > 0 else 0
-        x_vals.append(transaction["day"])
-        y_vals.append(round_float(last_amount + transaction["amount"]))
-    return [x_vals, y_vals]
+        last_amount = amounts[-1] if len(amounts) > 0 else 0
+        days.append(transaction["day"])
+        amounts.append(round_float(last_amount + transaction["amount"]))
+        descriptions.append(transaction["description"])
+        dates.append(calculate_date(start_date, transaction["day"]))
+    return (days, amounts, descriptions, dates)
 
 
 def round_float(num, precision=2):
@@ -34,3 +51,30 @@ def round_float(num, precision=2):
     Rounds the input number to the specified precision. Default is 2.
     """
     return round(num * 10 ** precision) / (10 ** precision)
+
+
+def calculate_date(date, day):
+    """
+    Calculates the next calendar date after `date` on which `day` will land. If
+    the calculated date falls outside the range of days for the month, the
+    highest day for that month will be returned.
+    """
+    increment_month = False
+    increment_year = False
+    if day == 0:
+        return date
+    if day < date.day:
+        increment_month = True
+    if increment_month & (date.month == 12):
+        increment_year = True
+
+    year = date.year + increment_year
+
+    month = date.month + increment_month
+    if month > 12:
+        month %= 12
+
+    _, max_day_of_month = monthrange(year, month)
+    day = day if day <= max_day_of_month else max_day_of_month
+
+    return datetime(year, month, day)
--- plot/test_plot_data.py
+++ plot/test_plot_data.py
@@ -1,15 +1,16 @@
 """
 Tests for plot_data
 """
-from plot_data import make_timeline, round_float, plot_data
+from datetime import datetime
+from plot_data import make_timeline, round_float, plot_data_plotly, calculate_date
 
 test_data = [
     {"description": "Current", "day": 0, "amount": 2000.00},
-    {"description": "Water Bill", "day": 3, "amount": -50},
     {"description": "Cell Phone", "day": 8, "amount": -80},
+    {"description": "Water Bill", "day": 3, "amount": -50},
+    {"description": "Savings", "day": 18, "amount": -100.00},
     {"description": "Mortgage", "day": 1, "amount": -1555.56},
     {"description": "Paycheck", "day": 15, "amount": 500.00},
-    {"description": "Savings", "day": 18, "amount": -100.00},
 ]
 
 
@@ -17,9 +18,28 @@ def test_make_timeline():
     """
     Test make_timeline function
     """
+    start_date = datetime(2021, 1, 1)
     expect_x = [0, 1, 3, 8, 15, 18]
     expect_y = [2000, 444.44, 394.44, 314.44, 814.44, 714.44]
-    assert [expect_x, expect_y] == make_timeline(test_data)
+    expect_desc = [
+        "Current",
+        "Mortgage",
+        "Water Bill",
+        "Cell Phone",
+        "Paycheck",
+        "Savings",
+    ]
+    expect_date = [
+        start_date,
+        datetime(2021, 1, 1),
+        datetime(2021, 1, 3),
+        datetime(2021, 1, 8),
+        datetime(2021, 1, 15),
+        datetime(2021, 1, 18),
+    ]
+    assert (expect_x, expect_y, expect_desc, expect_date) == make_timeline(
+        test_data, start_date
+    )
 
 
 def test_round_float():
@@ -30,4 +50,24 @@ def test_round_float():
     assert round_float(5.36598789756, 3) == 5.366
 
 
-plot_data(test_data)
+def test_calculate_date():
+    """
+    Test calculate_date
+    """
+    # Same day
+    assert calculate_date(datetime(2021, 1, 1), 0) == datetime(2021, 1, 1)
+    # Within same month
+    assert calculate_date(datetime(2021, 1, 1), 15) == datetime(2021, 1, 15)
+    # Next month
+    assert calculate_date(datetime(2021, 1, 28), 15) == datetime(2021, 2, 15)
+    # Next month and year
+    assert calculate_date(datetime(2021, 12, 28), 15) == datetime(2022, 1, 15)
+    # Day out of range
+    assert calculate_date(datetime(2021, 1, 30), 29) == datetime(2021, 2, 28)
+    # Day not out of range for leap year
+    assert calculate_date(datetime(2020, 1, 30), 29) == datetime(2020, 2, 29)
+    # On same day
+    assert calculate_date(datetime(2021, 1, 1), 1) == datetime(2021, 1, 1)
+
+
+# plot_data_plotly(test_data)
